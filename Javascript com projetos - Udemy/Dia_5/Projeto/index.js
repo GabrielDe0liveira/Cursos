34b4d4d85c14c85d611cadd1b0a47771622626be
Gabriel de Oliveira <gabriel.o99@outlook.com>
2025-03-28 09:24:15 -0300

    280325-1
    Efetuadas melhorias no projeto, logo implementado ainda mais coisas.

--- Javascript com projetos - Udemy/Dia_5/Projeto/index.js
+++ Javascript com projetos - Udemy/Dia_5/Projeto/index.js
@@ -1,3 +1,9 @@
+
+function formatMoney(value){
+    value = value.toFixed(2) //fixa o valor decimal em 2 casas
+    return "$ " + value
+}
+
 function update(){
     let bill = Number (document.getElementById("yourBill").value)
     let tipPercent = document.getElementById("tipInput").value
@@ -6,12 +12,16 @@ function update(){
     let tipValue = bill * (tipPercent / 100)
     let billTotal = bill + tipValue
     let billEach = billTotal / split
+    let tipEach = tipValue / split
+    let totalEach = billEach + tipEach
 
     document.getElementById("tipPercent").innerHTML = tipPercent + " %"
-    document.getElementById("tipValue").innerHTML = " $" + tipValue
-    document.getElementById("totalWithTip").innerHTML = " $" + billTotal
-    document.getElementById("billEach").innerHTML = billTotal / split
+    document.getElementById("tipValue").innerHTML = formatMoney(tipValue)
+    document.getElementById("totalWithTip").innerHTML = formatMoney(billTotal)
+    document.getElementById("billEach").innerHTML = formatMoney(billTotal / split)
     document.getElementById("splitValue").innerHTML = split
+    document.getElementById("tipEach").innerHTML = formatMoney(tipValue / split)
+    document.getElementById("totalEach").innerHTML = formatMoney(totalEach)
 }
 
 
